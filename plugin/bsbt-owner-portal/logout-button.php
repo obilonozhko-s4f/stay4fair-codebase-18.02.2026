a139743018c2a7cfa0735a653fb83df04cfeed78
obilonozhko-s4f <o.bilonozhko@googlemail.com>
2026-02-23 11:50:14 +0100

    Update logout-button.php

--- plugin/bsbt-owner-portal/logout-button.php
+++ plugin/bsbt-owner-portal/logout-button.php
@@ -92,9 +92,13 @@ add_shortcode('bsbt_logout_button', function() {
     </style>
     
     <div class="bsbt-logout-wrapper">
-        <a href="<?php echo esc_url(add_query_arg('action', 'owner_logout', home_url('/'))); ?>" class="bsbt-logout-link">
+        <a href="<?php echo esc_url( wp_nonce_url( add_query_arg('action', 'owner_logout', home_url('/')), 'bsbt_owner_logout' ) ); ?>" class="bsbt-logout-link">
             <span>Abmelden</span>
-            <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+            <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                <polyline points="16 17 21 12 16 7"></polyline>
+                <line x1="21" y1="12" x2="9" y2="12"></line>
+            </svg>
         </a>
     </div>
     <?php
